f8ff755f4c1c9057880efcbc457291042126d0d3
Andy Waldron <awaldron2@wisc.edu>
2020-03-08 14:06:27 -0500

    Trying to git.

---
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.firebasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
