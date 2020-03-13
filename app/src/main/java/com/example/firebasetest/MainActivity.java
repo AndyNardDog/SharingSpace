8822c09fa4251df4d7159487c126ba9c0a729ea8
Andy Waldron <awaldron2@wisc.edu>
2020-03-13 15:09:22 -0500

    Added firestore and authenticator.

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.firebasetest;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth authenticator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
