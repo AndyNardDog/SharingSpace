5ec6786b916a2a611ba7110754a8973a465e56a6
Dong Zhiheng <derricdong@saas.top>
2020-03-15 07:08:39 -0500

    Login page

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "LogIn";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth authenticator; // for the log-in, and DB information
 
@@ -25,23 +36,64 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         FirebaseUser user = authenticator.getCurrentUser();
-
-        userLoggedIn(user);
-    }
-
-    public void userLoggedIn(FirebaseUser user) {
-        if(user == null) {
-
-        } else {
-
+        if(user != null) {
+            gotoNext(user.getEmail());
         }
     }
+    public void onSignup(View view) {
+        EditText userEM = (EditText) findViewById(R.id.email);
+        final String userName = userEM.getText().toString();
+        EditText userPWD = (EditText) findViewById(R.id.passwords);
+        final String userPassword = userPWD.getText().toString();
+        authenticator.createUserWithEmailAndPassword(userName, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = authenticator.getCurrentUser();
+                            gotoNext(userName);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-    public void onLogin() {
-
+                });
     }
 
-    public void onSignUp() {
+
+    public void onLogin(View view) {
+        EditText userEM = (EditText) findViewById(R.id.email);
+        final String userName = userEM.getText().toString();
+        EditText userPWD = (EditText) findViewById(R.id.passwords);
+        final String userPassword = userPWD.getText().toString();
+        authenticator.signInWithEmailAndPassword(userName, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = authenticator.getCurrentUser();
+                            gotoNext(userName);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+
+    }
+    private void gotoNext(String userName) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
 
     }
 }
