e1285f4915b929f207ff411a4f7d363037d2db35
Kyle Kim <dkim577@wisc.edu>
2020-03-16 01:06:59 -0500

    Testing database

--- app/src/main/java/com/example/firebasetest/SellerPage.java
+++ app/src/main/java/com/example/firebasetest/SellerPage.java
@@ -9,18 +9,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SellerPage extends AppCompatActivity {
 
     EditText Address;
     EditText price;
     EditText description;
     Button register;
+    DatabaseReference databaseForSeller;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_page);
+
+        databaseForSeller = FirebaseDatabase.getInstance().getReference("SellerInfo");
 
         Address = (EditText) findViewById(R.id.Address);
         price = (EditText) findViewById(R.id.Price);
@@ -30,7 +36,7 @@ public class SellerPage extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addSellerInfo();
             }
         });
     }
@@ -44,7 +50,13 @@ public class SellerPage extends AppCompatActivity {
 
         if(!TextUtils.isEmpty(addressStr) || !TextUtils.isEmpty(priceStr)|| !TextUtils.isEmpty(descriptionStr)){
 
+          String sellerID =   databaseForSeller.push().getKey();
 
+         sellerData sellerinfo = new sellerData(  sellerID,  addressStr ,  priceStr ,  descriptionStr);
+
+         databaseForSeller.child(sellerID).setValue(sellerinfo);
+
+            Toast.makeText(this, "The parking lot is registered", Toast.LENGTH_LONG).show();
         }else{
             Toast.makeText(this, "You should fill out the all the blank", Toast.LENGTH_LONG).show();
 
--- app/src/main/java/com/example/firebasetest/sellerData.java
+++ app/src/main/java/com/example/firebasetest/sellerData.java
@@ -1,4 +1,44 @@
 package com.example.firebasetest;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class sellerData {
+
+    String sellerID;
+String address;
+String price;
+String description;
+
+
+    public sellerData(){}
+
+
+public sellerData( String sellerID, String address , String price , String description){
+        this.sellerID = sellerID;
+    this.address = address;
+    this.price = price;
+    this.description = description;
+
+
+}
+
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
 }
