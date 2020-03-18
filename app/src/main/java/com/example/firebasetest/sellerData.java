d997a97c9d3053f3295cefd93779be3b3be7b729
Kyle Kim <dkim577@wisc.edu>
2020-03-18 15:01:34 -0500

    backup Save parking info part 2

--- app/src/main/java/com/example/firebasetest/sellerData.java
+++ app/src/main/java/com/example/firebasetest/sellerData.java
@@ -1,4 +1,58 @@
 package com.example.firebasetest;
+import com.google.firebase.database.DatabaseReference;
 
 public class sellerData {
+
+    String sellerID;
+    String address;
+    String price;
+    String description;
+
+
+    public sellerData(){}
+
+
+    public sellerData( String sellerID, String address , String price , String description){
+        this.sellerID = sellerID;
+        this.address = address;
+        this.price = price;
+        this.description = description;
+
+
+    }
+
+    public void setSellerID(String sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
